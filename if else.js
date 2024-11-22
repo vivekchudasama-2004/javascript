f7b48db039074a5bc67d3e169c02d2654bfa07c4
Vivek Chudasama <127607552+vivekchudasama-2004@users.noreply.github.com>
2024-11-22 19:46:54 +0530

    Add files via upload

---
+++ if else.js
@@ -0,0 +1,18 @@
+let firstname ="vivek";
+const age=20;
+let ismarried=false;
+
+if (ismarried === true){
+    console.log(firstname + " is married ")
+}
+else {
+    console.log(firstname + " is not married ")
+}
+
+let i=101;
+if (i%2===0) {
+    console.log('it is even')
+}
+else{
+    console.log('it is odd')
+}
