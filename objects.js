e9cb07ab9bfd4f0b5133f5efd705b577cba05c1b
Vivek Chudasama <127607552+vivekchudasama-2004@users.noreply.github.com>
2024-11-22 19:47:53 +0530

    Add files via upload

---
+++ objects.js
@@ -0,0 +1,18 @@
+const user1 ={
+    firstname:"vivek",
+    gender:"male",
+
+    firstname2:"sahil",
+    gender2:"female",
+}
+console.log(user1["firstname"])
+console.log(user1['firstname2'])
+
+//arrays
+// const person1 =['vivek','sahil','mihir']
+// const gender =['male', 'female', 'male']
+// for(let i=0;i<=person1.length;i++) {
+//     if (gender[i] === 'female') {
+//         console.log(person1[i])
+//     }
+// }
