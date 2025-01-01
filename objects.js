19dc1a15100eafcefb6b657db1bbe0a9581f2d1a
Vivek Chudasama <127607552+vivekchudasama-2004@users.noreply.github.com>
2025-01-01 12:32:43 +0530

    Add files via upload

--- objects.js
+++ objects.js
@@ -1,12 +1,12 @@
-const user1 ={
-    firstname:"vivek",
-    gender:"male",
-
-    firstname2:"sahil",
-    gender2:"female",
-}
-console.log(user1["firstname"])
-console.log(user1['firstname2'])
+// const user1 ={
+//     firstname:"vivek",
+//     gender:"male",
+//
+//     firstname2:"sahil",
+//     gender2:"female",
+// }
+// console.log(user1["firstname"])
+// console.log(user1['firstname2'])
 
 //arrays
 // const person1 =['vivek','sahil','mihir']
@@ -16,3 +16,38 @@ console.log(user1['firstname2'])
 //         console.log(person1[i])
 //     }
 // }
+
+
+function greet(user){
+    console.log("HI " + user.gender + user.name + " your age : "+user.age);
+}
+
+let user ={
+    name: "vivek",
+    age : 21,
+    gender: "MR."
+}
+
+greet(user);
+
+function user1(users) {
+    users.forEach(function(user) {
+        if (user.age < 19) {
+            console.log("Hello: " + user.name + " your age is: " + user.age);
+        } else {
+            console.log(user.name + " is above 18");
+        }
+    });
+}
+
+const users = [{
+    name: "vivek",
+    age: 21,
+    gender: "male"
+}, {
+    name: "jhanvi",
+    age: 21,
+    gender: "female"
+}];
+
+user1(users);
