8740e79d02b425c9a7f0e0bc12a2b64813ecc41f
Vivek Chudasama <127607552+vivekchudasama-2004@users.noreply.github.com>
2025-01-02 11:41:04 +0530

    Delete more in variable.js

--- more in variable.js
+++
@@ -1,9 +0,0 @@
-let firstname ="vivek";
-let age=20;
-let ismarried=false;
-
-console.log("this person name is "+ firstname + "and his age is"
-+ age + "is he married"+ ismarried);
-
-console.log("name of person :" + firstname)
-console.log("his age is :" +age)
