1dc395a69ec38287c69df33f8dfc576fac610e63
Vivek Chudasama <127607552+vivekchudasama-2004@users.noreply.github.com>
2025-01-02 11:50:47 +0530

    Add files via upload

---
+++ variable/variable.js
@@ -0,0 +1,13 @@
+//it is jast an valiable
+var a =1;
+console.log(a);
+
+//variable which can be changable
+let b=1;
+b=2;
+console.log(b);
+
+//variable which cannot be changable by othe variable it throw error if any variable change it
+const c=1;
+c=2;
+console.log(c);
