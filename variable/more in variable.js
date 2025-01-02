2998dfd62f1a03e82f65fb24c9491026c3ed9dd7
Vivek Chudasama <127607552+vivekchudasama-2004@users.noreply.github.com>
2025-01-02 11:50:33 +0530

    Add files via upload

---
+++ variable/more in variable.js
@@ -0,0 +1,9 @@
+let firstname ="vivek";
+let age=20;
+let ismarried=false;
+
+console.log("this person name is "+ firstname + "and his age is"
++ age + "is he married"+ ismarried);
+
+console.log("name of person :" + firstname)
+console.log("his age is :" +age)
