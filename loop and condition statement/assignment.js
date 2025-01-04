ead93b542740da5f1568033fea452794d44f8bfe
Vivek Chudasama <127607552+vivekchudasama-2004@users.noreply.github.com>
2025-01-04 19:32:05 +0530

    Add files via upload

---
+++ loop and condition statement/assignment.js
@@ -0,0 +1,22 @@
+function countdown() {
+    let num = 30;
+    while (num > 0) {
+
+        console.log(num)
+        num--;
+    }
+}
+countdown();
+
+
+
+function countdown()
+{
+    let num=30;
+    for (let i=30;i>=0;i--)
+    {
+        console.log(num)
+        num--;
+    }
+}
+countdown();
