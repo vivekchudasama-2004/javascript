3c3417d48e152b35a86544fa0167938528a82881
Vivek Chudasama <127607552+vivekchudasama-2004@users.noreply.github.com>
2025-01-09 18:05:53 +0530

    Add files via upload

---
+++ function call back/function call back second.js
@@ -0,0 +1,8 @@
+function greet(a,b){
+    console.log("hellow world")
+}
+function greetalian(a,b,callback){
+    console.log("hello alian")
+}
+setTimeout(greet,3*1000)
+//setTimeout(greetalian,3*1000)
