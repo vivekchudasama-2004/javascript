76451c9df0db4449815080f06fa7546703686f2c
Vivek Chudasama <127607552+vivekchudasama-2004@users.noreply.github.com>
2025-01-10 18:58:02 +0530

    Add files via upload

---
+++ assignment/print revese order.js
@@ -0,0 +1,14 @@
+let num=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+for(let i=num.length;i>=0;i--){
+    console.log(num[i]);
+}
+
+let name=["vivek","sahil","jhanvi"];
+for(let i=num.length-1;i>=0;i--){
+    console.log(name[i]);
+}
+
+function sum(a,b){
+    return a + b;
+}
+console.log(sum(1,2));
