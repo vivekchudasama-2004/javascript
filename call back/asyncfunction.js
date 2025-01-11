428689461e39959cc7f9f926147396c2c9b1ecf2
Vivek Chudasama <127607552+vivekchudasama-2004@users.noreply.github.com>
2025-01-11 19:18:17 +0530

    Add files via upload

---
+++ call back/asyncfunction.js
@@ -0,0 +1,34 @@
+// const fs = require('fs');
+//
+// function print (err ,  data){
+//     if (err) throw err;
+//     else console.log(data)
+// }
+//
+// fs.readFile('asyncfunction.js', 'utf-8', print)
+//
+// fs.readFile('b.txt', 'utf-8', print)
+//
+// console.log('done')
+//
+
+
+function timeout(){
+    console.log('function hear');
+}
+
+console.log('hi there');
+
+setTimeout(timeout, 10000);
+
+console.log('hi ');
+// first loop get run and then time out get print because
+// for loop take the thread to run because javascript is single threaded language
+
+let c=0;
+
+for (let i=0 ; i<10000000000; i++){
+    c=c+i
+}
+console.log(c);
+console.log('expensive operation')
