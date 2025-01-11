7598fc2fa1d0b811c12ad20a24265d592bf93a72
Vivek Chudasama <127607552+vivekchudasama-2004@users.noreply.github.com>
2025-01-11 19:18:40 +0530

    Add files via upload

---
+++ call back/built in classes.js
@@ -0,0 +1,17 @@
+let time = new Date();
+console.log(time.getTime());
+console.log(time.getHours());
+console.log(time.getMinutes());
+console.log(time.getSeconds());
+
+console.log(time.getTimezoneOffset())
+
+
+
+const map = new Map();
+map.set('name','xyx');
+map.set('age','21');
+
+console.log('Get the name '+ map.get('name'));
+console.log('Get the age ' + map.get('age'));
+
