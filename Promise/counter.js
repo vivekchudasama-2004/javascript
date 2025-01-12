f508911484a4a48f52aaebe9ea8898c9b0f2595f
Vivek Chudasama <127607552+vivekchudasama-2004@users.noreply.github.com>
2025-01-12 22:09:17 +0530

    Add files via upload

---
+++ Promise/counter.js
@@ -0,0 +1,21 @@
+let counter=0;
+
+function count (){
+    counter++;
+    console.log(counter);
+}
+
+setInterval(count,1000)
+
+//or
+
+
+// let counter = 0;
+//
+//
+// const updateCounter = () => {
+//     counter++;
+//     console.log(counter);
+// };
+//
+// setInterval(updateCounter, 1000);
