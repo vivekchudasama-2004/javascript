5bda525568510fd285345b28eae85631493070cd
Vivek Chudasama <127607552+vivekchudasama-2004@users.noreply.github.com>
2025-01-12 22:11:03 +0530

    Update counter.js

--- Promise/counter.js
+++ Promise/counter.js
@@ -10,12 +10,12 @@ setInterval(count,1000)
 //or
 
 
-// let counter = 0;
-//
-//
-// const updateCounter = () => {
-//     counter++;
-//     console.log(counter);
-// };
-//
-// setInterval(updateCounter, 1000);
+let counter = 0;
+
+
+const updateCounter = () => {
+    counter++;
+    console.log(counter);
+};
+
+setInterval(updateCounter, 1000);
